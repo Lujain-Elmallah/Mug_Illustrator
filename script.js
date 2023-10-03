80b019deeaeceaef901e868de0da733788c7a9b2
Lujain-Elmallah <144986744+Lujain-Elmallah@users.noreply.github.com>
2023-10-03 22:32:11 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the drink element by its id
     var drink = document.getElementById("drink-path");
 
-    // Function to generate a random color
     function getRandomColor() {
         var letters = "0123456789ABCDEF";
         var color = "#";
@@ -12,9 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
         return color;
     }
 
-    // Add a click event listener to the drink element
     drink.addEventListener("click", function() {
-        // Change the fill color of the drink to a random color
         var randomColor = getRandomColor();
         drink.style.fill = randomColor;
     });
